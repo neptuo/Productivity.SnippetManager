6dafdb20dcf115c627515b94a6d8f8f1854d1ae5
Marek Fišera <mara@neptuo.com>
2022-09-27 15:06:31 +0200

    Tray icon.

--- src/Neptuo.Productivity.SnippetManager.UI/App.xaml.cs
+++ src/Neptuo.Productivity.SnippetManager.UI/App.xaml.cs
@@ -3,16 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
+using Application = System.Windows.Application;
 
 namespace Neptuo.Productivity.SnippetManager
 {
     public partial class App : Application
     {
         private readonly Navigator navigator = new Navigator(new CompositeSnippetProvider(new DemoSnippetProvider(), new GuidSnippetProvider()));
+        private NotifyIcon? trayIcon;
 
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -21,6 +26,27 @@ namespace Neptuo.Productivity.SnippetManager
             {
                 navigator.OpenMain();
             });
+
+            CreateTrayIcon();
+        }
+
+        private void CreateTrayIcon()
+        {
+            trayIcon = new NotifyIcon
+            {
+                Icon = Icon.ExtractAssociatedIcon(Process.GetCurrentProcess().MainModule!.FileName!),
+                Text = "Snippet Manager"
+            };
+            trayIcon.MouseClick += (sender, e) =>
+            {
+                if (e.Button != MouseButtons.Right) 
+                    navigator.OpenMain();
+            };
+            trayIcon.Visible = true;
+
+            trayIcon.ContextMenuStrip = new ContextMenuStrip();
+            trayIcon.ContextMenuStrip.Items.Add("Open").Click += (sender, e) => { navigator.OpenMain(); };
+            trayIcon.ContextMenuStrip.Items.Add("Exit").Click += (sender, e) => Shutdown();
         }
     }
 }
