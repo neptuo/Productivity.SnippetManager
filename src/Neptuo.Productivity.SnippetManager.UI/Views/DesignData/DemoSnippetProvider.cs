0e40060e155e2d392395501922af57e3f10c4b02
Marek Fišera <mara@neptuo.com>
2022-10-03 14:30:16 +0200

    Fix design data.

---
+++ src/Neptuo.Productivity.SnippetManager.UI/Views/DesignData/DemoSnippetProvider.cs
@@ -0,0 +1,50 @@
+﻿using Neptuo.Productivity.SnippetManager.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Productivity.SnippetManager.Views.DesignData;
+
+internal class DemoSnippetProvider : ISnippetProvider
+{
+    public Task<IReadOnlyCollection<SnippetModel>> GetAsync()
+    {
+        List<SnippetModel> result = new();
+
+        void Add(string title, string text, string? description = null)
+            => result.Add(new SnippetModel(title, text, description));
+
+        Add(
+            "C# class",
+            """
+            using System;
+            using System.Collections.Generic;
+            using System.IO;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+            namespace Namespace
+            {
+                public class Class
+                {
+                }
+            }
+            """,
+            "public class Class..."
+        );
+        Add("Google", "https://google.com");
+        Add("Maps", "https://maps.google.com");
+        Add("GitHub - dotnet - runtime", "https://github.com/dotnet/runtime");
+        Add("GitHub - Maraf - Money", "https://github.com/maraf/money");
+        Add("GitHub - Neptuo - Recollections", "https://github.com/neptuo/Recollections");
+        Add("Money", "https://app.money.neptuo.com");
+        Add("Recollections", "https://recollections.app");
+        Add("Signature", $"S pozdravem{Environment.NewLine}Marek Fišera");
+
+        return Task.FromResult<IReadOnlyCollection<SnippetModel>>(result);
+    }
+}
