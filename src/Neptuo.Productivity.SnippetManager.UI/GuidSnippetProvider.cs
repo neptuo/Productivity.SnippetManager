346350071067543316d4d55a4f2ba0cc2bffa75a
Marek Fišera <mara@neptuo.com>
2022-10-11 10:31:31 +0200

    Drop previous GUID snippet.

--- src/Neptuo.Productivity.SnippetManager.UI/GuidSnippetProvider.cs
+++ src/Neptuo.Productivity.SnippetManager.UI/GuidSnippetProvider.cs
@@ -10,12 +10,18 @@ namespace Neptuo.Productivity.SnippetManager;
 
 public class GuidSnippetProvider : ISnippetProvider
 {
+    private const string Title = "GUID";
+
     public Task InitializeAsync(SnippetProviderContext context) 
         => Task.CompletedTask;
 
     public Task UpdateAsync(SnippetProviderContext context)
     {
-        context.Models.Add(SnippetModel.SingleCollection("GUID", Guid.NewGuid().ToString()));
+        var existing = context.Models.FirstOrDefault(m => m.Title == Title);
+        if (existing != null)
+            context.Models.Remove(existing);
+
+        context.Models.Add(new SnippetModel(Title, Guid.NewGuid().ToString()));
         return Task.CompletedTask;
     }
 }
