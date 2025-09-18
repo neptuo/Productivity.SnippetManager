69ee6b55deadd99f24ba309d2d4cb29fe1e528e8
Marek Fišera <mara@neptuo.com>
2025-09-18 15:12:55 +0200

    Read clipboard history and create snippets from all items

--- src/Neptuo.Productivity.SnippetManager.UI/ClipboardSnippetProvider.cs
+++ src/Neptuo.Productivity.SnippetManager.UI/ClipboardSnippetProvider.cs
@@ -1,5 +1,6 @@
-﻿using Neptuo.Productivity.SnippetManager.Models;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using Neptuo.Productivity.SnippetManager.Models;
+using Clipboard2 = Windows.ApplicationModel.DataTransfer.Clipboard;
 
 namespace Neptuo.Productivity.SnippetManager;
 
@@ -7,7 +8,7 @@ public class ClipboardSnippetProvider : TransientSnippetProvider
 {
     private const string Title = "Text from Clipboard";
 
-    public override Task UpdateAsync(SnippetProviderContext context)
+    public override async Task UpdateAsync(SnippetProviderContext context)
     {
         var existing = context.Models.FirstOrDefault(m => m.Title == Title);
         if (existing != null)
@@ -20,6 +21,11 @@ public class ClipboardSnippetProvider : TransientSnippetProvider
                 context.Add(new SnippetModel(Title, text, priority: SnippetPriority.Most));
         }
 
-        return Task.CompletedTask;
+        var items = await Clipboard2.GetHistoryItemsAsync();
+        foreach (var item in items.Items.OrderByDescending(i => i.Timestamp))
+        {
+            string text = await item.Content.GetTextAsync();
+            context.Add(new SnippetModel($"{Title} - {text.Trim()}", text));
+        }
     }
 }
